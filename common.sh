40928f4284921f31ba61a651b409cef21fbbcd2d
OshVanK <98622870+Edsny1@users.noreply.github.com>
2025-02-03 22:58:39 +0300

    Update common.sh

--- common.sh
+++ common.sh
@@ -5,7 +5,7 @@ YELLOW="\e[1m\e[1;33m"  # yellow color
 NC="\e[0m"          # no color
 
 function printLogo {
-  bash <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/Edsny1/Tech/refs/heads/Edsny/logo.sh)
 }
 
 function printLine {
