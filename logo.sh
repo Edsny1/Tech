8581798c644f4505fd728e5a8066eaa03a4bf793
OshVanK <98622870+Edsny1@users.noreply.github.com>
2025-02-03 22:49:59 +0300

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,12 @@
+
+echo -e "\033[0;34m"
+echo "    .g8""8q.    .M"""bgd `7MMF'  `7MMF'`7MMF'   `7MF'      db      `7MN.   `7MF'`7MMF' `YMM'   ";
+echo "  .dP'    `YM. ,MI    "Y   MM      MM    `MA     ,V       ;MM:       MMN.    M    MM   .M'     ";
+echo "  dM'      `MM `MMb.       MM      MM     VM:   ,V       ,V^MM.      M YMb   M    MM .d"       ";
+echo "  MM        MM   `YMMNq.   MMmmmmmmMM      MM.  M'      ,M  `MM      M  `MN. M    MMMMM.       ";
+echo "  MM.      ,MP .     `MM   MM      MM      `MM A'       AbmmmqMA     M   `MM.M    MM  VMA      ";
+echo "  `Mb.    ,dP' Mb     dM   MM      MM       :MM;       A'     VML    M     YMM    MM   `MM.    ";
+echo "    `"bmmd"'   P"Ybmmd"  .JMML.  .JMML.      VF      .AMA.   .AMMA..JML.    YM  .JMML.   MMb.  ";
+echo -e "\033[0;32m"
+echo "                                                                       https://oshvank.com/"
+echo -e "\e[0m"
